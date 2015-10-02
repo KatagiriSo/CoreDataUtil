1b9553088b322042bf92ed62956dd44519677a38
Katagiri <katagiri@plala.co.jp>
2015-10-02 10:30:17 +0900

    Initial Commit

---
+++ CoreDataUtil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataUtil
+//
+//  Created by 片桐奏羽 on 2015/10/02.
+//  Copyright (c) 2015年 SoKatagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
